b04043a365264382eb3182f8aedd67a8e827aec5
MisterBigThor <vcr9713@gmail.com>
2020-03-17 13:20:13 +0100

    single OK

--- src/single.c
+++ src/single.c
@@ -6,11 +6,19 @@ miniomp_single_t miniomp_single;
 /* This routine is called when first encountering a SINGLE construct. 
    Returns true if this is the thread that should execute the clause.  */
 
-bool
-GOMP_single_start (void)
+bool GOMP_single_start (void)
 {
-  printf("TBI: Entering into single, but don't know who else arrived so I proceed\n");
-  return(true);
+	if(__sync_bool_compare_and_swap(&miniomp_single.singleActivation, false, true)){
+		miniomp_single.idSingleThread = omp_get_thread_num();
+		printf("SINGLE: YES!,I'm the single one \n");
+		return(true);
+	}
+	else {
+		printf("SINGLE: I'm not the single one :( \n");
+		return(false);
+	}
+//  	printf("TBI: Entering into single, but don't know who else arrived so I proceed\n");
+
 }
 
 
--- src/single.h
+++ src/single.h
@@ -1,6 +1,9 @@
 // Type declaration for single work sharing descriptor
 typedef struct {
-    // complete the definition of single descriptor
+	bool singleActivation; 	//var for control the init.
+	int idSingleThread; 	//only for info, usefull?
+
+	// complete the definition of single descriptor
 } miniomp_single_t;
 
 // Declaration of global variable for single work descriptor
