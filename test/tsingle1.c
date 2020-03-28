073ec6031fbced1c0e7e500852ed27cd68b97a7c
MisterBigThor <vcr9713@gmail.com>
2020-03-28 23:04:02 +0100

    added

---
+++ test/tsingle1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <string.h>
+#include <omp.h>	/* OpenMP */
+
+
+
+int main(int argc, char *argv[]) {
+	#pragma omp parallel
+	#pragma omp single
+	{
+		printf("Only one thread here, I'm %u!\n", omp_get_thread_num());
+	}
+	printf("Everyone must print this\n");
+}
