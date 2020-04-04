61982512992ad6e4ceb64959ff5dfc5f0b978223
Victor Correal Ramos <35953130+MisterBigThor@users.noreply.github.com>
2020-04-04 06:23:38 +0200

    Update Tsingle.c

--- test/Tsingle.c
+++ test/Tsingle.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -8,36 +7,11 @@
 #include <omp.h>	/* OpenMP */
 
 
-int main(int argc, char *argv[]) {
-	printf("Test 1: Parallel with a single");
-	int i = 4;
-	#pragma omp parallel
-	{
-		#pragma omp single
-		{
-			printf("Only one thread here, I'm %u!\n", omp_get_thread_num());
-			i += 4;
-		}
-		//printf("Hi!, I'm thread %u\n", omp_get_thread_num());
-	}
-	printf("Expected 8, result: %u\n", i);
-	printf("****====****====****====****\n");
-	printf("Test 2: Parallel single with a no wait\n");
-	#pragma omp parallel
-	{
-		#pragma omp single nowait
-		{
-			sleep(3);
-			printf("Last message: Only one thread here!\n");
-//			printf("Last message: Only one sleep here here, I'm %u!\n", omp_get_thread_num());
 
-		}
+int main(int argc, char *argv[]) {
+	#pragma omp parallel
+	for(int i = 0; i< 10; ++i){
 		#pragma omp single nowait
-		{
-			printf("Second single. Only one thread here\n");
-//			printf("Secons single: Only one thread here, I'm %u!\n", omp_get_thread_num());
-		}
-		//printf("Hi!, I'm thread %u\n", omp_get_thread_num());
+		sleep(3);
 	}
-	printf("****====****====****====****\n");
 }
