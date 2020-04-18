cef30db78c022eaf37ccfb209a07ad2c24b45087
Victor Correal Ramos <35953130+MisterBigThor@users.noreply.github.com>
2020-04-18 14:04:17 +0200

    Update parallel.h

--- src/parallel.h
+++ src/parallel.h
@@ -3,7 +3,6 @@
 
 #include <pthread.h>
 
-// Declaration of array for storing pthread identifiers from pthread_create function
 extern pthread_t *miniomp_threads;
 
 // Type declaration for parallel descriptor (arguments needed to create pthreads)
@@ -11,19 +10,15 @@ typedef struct {
 	void (*fn) (void *);
 	void *fn_data;
 	int id;
-//	int nestedLevel;
-	// complete the definition of parallel descriptor
+	int used;
 } miniomp_parallel_t;
 
 extern miniomp_parallel_t *miniomp_parallel;
 
-// Declaration of per-thread specific key
 extern pthread_key_t miniomp_specifickey;
 
-// Declaration of per-thread specific key for data:
 extern pthread_key_t miniomp_internalkey;
 
-// Functions implemented in this module
 void GOMP_parallel (void (*fn) (void *), void *data, unsigned num_threads, unsigned int flags);
 
 #endif
